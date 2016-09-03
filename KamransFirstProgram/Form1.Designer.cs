b2a3735990077ea43e6cfdc5a3a56f92755a7559
im281 <iman.mohtashemi@gmail.com>
2016-09-03 09:45:46 -0700

    1st counting excersises

--- KamransFirstProgram/Form1.Designer.cs
+++ KamransFirstProgram/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(87, 101);
+            this.button1.Location = new System.Drawing.Point(75, 81);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 21);
+            this.button1.Size = new System.Drawing.Size(123, 87);
             this.button1.TabIndex = 0;
             this.button1.Text = "Hi Kamran, Press me!";
             this.button1.UseVisualStyleBackColor = true;
--- KamransFirstProgram/Form1.cs
+++ KamransFirstProgram/Form1.cs
@@ -19,9 +19,37 @@ namespace KamransFirstProgram
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Our1stMethod();
+        }
+
+        private static void Our1stMethod()
+        {
             MessageBox.Show("Hi Kamran, Its Daddy!");
             MessageBox.Show("find the diamond sword");
-            //test1
+            MessageBox.Show("find block");
+
+            for (int i = 0; i < 100000; i++)
+            {
+                MessageBox.Show("What is the answer to:" + i + " " + "+" + 3 + " ?");
+                MessageBox.Show(Convert.ToString(i + 3));
+
+                if (i == 10)
+                {
+                    MessageBox.Show("You have reached 10!!!!!!");
+                }
+                if (i == 15)
+                {
+                    MessageBox.Show("its count down time");
+                    for (int j = 15; j > 0; j--)
+                    {
+                        MessageBox.Show(Convert.ToString(j));
+
+                    }
+                    break;
+                }
+            }
+
+            MessageBox.Show("We are DONE@!!!!!!!!!");
         }
     }
 }
