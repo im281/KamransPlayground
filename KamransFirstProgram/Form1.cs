c7c8a2ee01cf3895d8df1bdc47a959dab4b19c1e
im281 <iman.mohtashemi@gmail.com>
2016-09-10 11:12:19 -0700

    Created loop class

--- KamransFirstProgram/Form1.cs
+++ KamransFirstProgram/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KamransFirstProgram.Lessons.Loops;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,46 +20,9 @@ namespace KamransFirstProgram
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Our1stMethod();
-        }
-
-        private static void Our1stMethod()
-        {
-
-            for (int i = 0; i < 10; i++) 
-            {
-                MessageBox.Show("i did   it ");
-            }
-
-            MessageBox.Show("hit the other team");
-
-
-            MessageBox.Show("Hi Kamran, Its Daddy!");
-            MessageBox.Show("find the diamond sword");
-            MessageBox.Show("find block");
-
-            for (int i = 0; i < 10; i++)
-            {
-                MessageBox.Show("What is the answer to:" + i + " " + "+" + 3 + " ?");
-                MessageBox.Show(Convert.ToString(i + 3));
-
-                if (i == 10)
-                {
-                    MessageBox.Show("You have reached 10!!!!!!");
-                }
-                if (i == 15)
-                {
-                    MessageBox.Show("its count down time");
-                    for (int j = 15; j > 0; j--)
-                    {
-                        MessageBox.Show(Convert.ToString(j));
-
-                    }
-                    break;
-                }
-            }
-
-            MessageBox.Show("We are DONE@!!!!!!!!!");
-        }
+            Loops k = new Loops();
+            k.GO();
+        }       
     }
 }
+ 
---
+++ KamransFirstProgram/Lessons/Loops/Loops.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KamransFirstProgram.Lessons.Loops
+{
+    public class Loops
+    {
+        public void GO()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("i did   it ");
+            }
+
+            MessageBox.Show("hit the other team");
+
+
+            MessageBox.Show("Hi Kamran, Its Daddy!");
+            MessageBox.Show("find the diamond sword");
+            MessageBox.Show("find block");
+
+            for (int i = 0; i < 10; i++)
+            {
+                MessageBox.Show("What is the answer to:" + i + " " + "+" + 3 + " ?");
+                MessageBox.Show(Convert.ToString(i + 3));
+
+                if (i == 10)
+                {
+                    MessageBox.Show("You have reached 10!!!!!!");
+                }
+                if (i == 15)
+                {
+                    MessageBox.Show("its count down time");
+                    for (int j = 15; j > 0; j--)
+                    {
+                        MessageBox.Show(Convert.ToString(j));
+
+                    }
+                    break;
+                }
+            }
+
+            MessageBox.Show("We are DONE@!!!!!!!!!");
+        }
+    }
+}
+ 
